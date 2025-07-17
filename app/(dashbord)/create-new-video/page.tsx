3b6d032e3a9a9051cad0e63be5f946d43f729c49
Ashish7kumar <ashish11to12@gmail.com>
2025-07-18 04:28:55 +0530

    added db to save video info connected audio generation to sript generationa and other things

--- app/(dashbord)/create-new-video/page.tsx
+++ app/(dashbord)/create-new-video/page.tsx
@@ -6,10 +6,22 @@ import Voices from './components/Voices'
 import Caption from './components/Caption'
 import axios from 'axios'
 import { Button } from '@/components/ui/button'
-import { WandSparkles } from 'lucide-react'
+import { Loader2Icon, WandSparkles } from 'lucide-react'
 import Preview from './components/Preview'
+import { useMutation } from 'convex/react'
+import { useAuthContext } from '@/app/themeProvider'
+import { api } from '@/convex/_generated/api'
+
 const CreateNewVideo = () => {
+  const [loading,setLoading]=useState(false);
     const [formData,setFormData]=useState<any>({});
+    const context=useAuthContext();
+      if(context === null)
+      {
+        throw Error('No user context')
+      }
+      const {user}=context;
+    const CreateInitialVideoRecord=useMutation(api.videoData.CreateVideoData)
    const onHandleInputChange = (fieldName: string, fieldValue: any) => {
     setFormData((prev: any) => ({
       ...prev,
@@ -18,16 +30,28 @@ const CreateNewVideo = () => {
     console.log(formData);
   };
    const GenerateVideo=async ()=>{
+    setLoading(true);
     console.log(formData)
 if (!formData?.topic || !formData?.script || !formData?.videoStyle || !formData?.caption || !formData?.voice) {
   console.error("Error: fields are missing");
   return;
 }
-
+    const resp=await CreateInitialVideoRecord({
+      title:formData.title,
+      topic:formData.topic,
+      script:formData.script,
+      videoStyle:formData.videoStyle,
+      caption:formData.voice,
+      voice:formData.voice,
+      uid:user?._id,
+      createdBy:user?.email
+    })
+    console.log(resp);
     const result=await axios.post('/api/generate-video',{
       ...formData
     });
     console.log(result);
+    setLoading(false)
    }
   return (
     <div>
@@ -38,7 +62,21 @@ if (!formData?.topic || !formData?.script || !formData?.videoStyle || !formData?
      <VideoStyle onHandleInputChange={onHandleInputChange}/>
      <Voices onHandleInputChange={onHandleInputChange}/>
      <Caption onHandleInputChange={onHandleInputChange}/>
-     <Button className='w-full mt-5 cursor-pointer text-white bg-purple-500 hover:bg-purple-700' onClick={GenerateVideo}><WandSparkles/>Generate Video</Button>
+     <Button
+  className='w-full mt-5 cursor-pointer text-white bg-purple-500 hover:bg-purple-700 flex items-center justify-center gap-2'
+  disabled={loading}
+  onClick={GenerateVideo}
+>
+  {loading ? (
+    <Loader2Icon className='animate-spin' />
+  ) : (
+    <>
+      <WandSparkles />
+      Generate Video
+    </>
+  )}
+</Button>
+
   </div>
 <div>
 <Preview formData={formData}/>
